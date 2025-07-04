d08182cf753a52e3e436deb1caeb5c1d3d266a63
Hao Le <leduchao77g1@gmail.com>
2025-07-04 19:41:50 +0700

    Update Home and Products pages with content and data fetching logic

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+
+import { useEffect } from "react";
+
 export default function Products() {
+  const fetchProducts = async () => {
+    const data = await fetch("https://dummyjson.com/products");
+    console.log("Products data:", await data.json());
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return <div className="">Products Page</div>;
 }
